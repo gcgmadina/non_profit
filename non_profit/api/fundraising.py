6979ee626cc384ed63bf5a6feca57473ba749e66
Salman <alfariqsalman@gmail.com>
2024-03-04 19:16:21 +0700

    menampilkan jumlah seluruh donasi dan jumatan

--- non_profit/api/fundraising.py
+++ non_profit/api/fundraising.py
@@ -1,20 +1,23 @@
 import frappe
+import datetime
+import json
 
 @frappe.whitelist(allow_guest=True)
 def get_total_amount():
+    today = datetime.date.today()
+    first = today.replace(day=1)
     try:
         # Use frappe.db.sql to execute SQL query
         total_amount = frappe.db.get_value("Donation",
-                                            {"item_type": ("!=", "Barang")},
+                                            {"item_type": ("!=", "Barang"),
+                                             "date": ("between", [first, today])},
                                             "sum(amount) as total")
-        print(total_amount)
-        return total_amount or 0  # Return 0 if total_amount is None
+        return str(total_amount or 0)  # Return 0 if total_amount is Non
     except Exception as e:
         frappe.log_error("Error in get_total_amount: {0}".format(str(e)))
-        print("error")
         return 0  # Return 0 in case of an error
 
 @frappe.whitelist(allow_guest=True)
-def get_infaq_danations():
-    total_amount = frappe.db.get_value("donation", {"donation_type": "Infaq"}, "sum(amount)", as_dict=True)
+def get_jumatan_donations():
+    total_amount = frappe.db.get_value("Donation", {"donation_type": "Jumatan"}, "amount")
     return total_amount
